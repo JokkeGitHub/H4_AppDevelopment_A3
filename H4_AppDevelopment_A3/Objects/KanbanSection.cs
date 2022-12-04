dd16aa6cb72c1e84d8e8627f7bad9b15c6c7c8ae
Joachim Hansen <skraldespand94@hotmail.com>
2022-12-04 19:36:02 +0100

    Add project files.

---
+++ H4_AppDevelopment_A3/Objects/KanbanSection.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace H4_AppDevelopment_A3
+{
+    public class KanbanSection
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; init; }
+        [JsonPropertyName("newTaskOpen")]
+        public bool NewTaskOpen { get; set; }
+        [JsonPropertyName("newTaskName")]
+        public string NewTaskName { get; set; }
+
+        public KanbanSection(string name, bool newTaskOpen, string newTaskName)
+        {
+            Name = name;
+            NewTaskOpen = newTaskOpen;
+            NewTaskName = newTaskName;
+        }
+    }
+}
---
+++ H4_AppDevelopment_A3/Objects/KanbanTaskItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace H4_AppDevelopment_A3
+{
+    public class KanbanTaskItem
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; init; }
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+
+        public KanbanTaskItem(string name, string status)
+        {
+            Name = name;
+            Status = status;
+        }
+    }
+}
